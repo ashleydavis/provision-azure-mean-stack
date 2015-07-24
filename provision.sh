be06504b4582dd881ef2eecb24ca79af8742954d
Ashley Davis <ashley.davis@realseriousgames.com>
2015-07-24 16:47:17 +1000

    Added commands to the shell script that prep a Mercurial repo for accepting code deployments.

--- provision.sh
+++ provision.sh
@@ -16,4 +16,11 @@ sudo pm2 startup ubuntu -u $USER
 echo "============== Fixing mongodb conf =================="
 sudo sed -i 's/bind_ip = 127.0.0.1/#bind_ip  = 127.0.0.1/g' /etc/mongodb.conf
 sudo service mongodb restart
-echo "============== Provisioning Finshed =================="
+echo "============== Creating deployment repo =================="
+mkdir deployment
+cd deployment
+hg init
+cd .hg
+echo "[hooks]" >> hgrc
+echo "changegroup = hg up >> ./logs/update.log; chmod +x ./Deploy/server_restart.sh; ./Deploy/server_restart.sh" >> hgrc
+echo "============== Provisioning Finished =================="
